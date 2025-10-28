6eec5696f27732a8dddb668db1f0b998aa4a34cc
BXn4 <bencefodor@proton.me>
2025-10-28 12:23:55 +0100

    Fancy rewards for some achievements

--- internal/managers/achievement.go
+++ internal/managers/achievement.go
@@ -3,6 +3,7 @@ package managers
 import (
 	"cafego/internal/utils"
 	"fmt"
+	"math/rand"
 )
 
 func (gm *GameManager) SendEarnAchievement(achievementWOD int, level int, username string) {
@@ -21,6 +22,30 @@ func (gm *GameManager) SendEarnAchievement(achievementWOD int, level int, userna
 	c.Player.AddXP(achievementXPReward)
 	c.Player.AddCash(achievementCashReward)
 	c.Player.AddGold(achievementGoldReward)
+
+	// 2005 -> boughtingredients
+	// 2013 -> coopgoldcount
+	// 2014 -> 2019 total dish category cooked
+	// 2020 -> total dishes cooked
+	// 2021 -> different dishes
+	// 2022 -> masteries
+	// 2023 -> masteries gold
+	// 2024 -> fancy count
+	if achievementWOD == 2005 || achievementID >= 2013 && achievementID <= 2024 {
+		fancies, err := utils.GetItems("fancy")
+		if err != nil {
+			return
+		}
+		choice := rand.Intn(len(fancies))
+		fancy := fancies[choice]
+		amount := 10
+
+		args = fmt.Sprintf("%s+%d+%d", args, fancy.ID, amount)
+
+		c.Location.Cafe().AddToFridge(fancy.ID, amount)
+
+		c.DB.UpdateFridgeInventory(c.Location.Cafe().ID, c.Location.Cafe().FridgeInventory.String())
+	}
 
 	c.DB.UpdateXP(c.Player.ID, c.Player.GetXP())
 	c.DB.UpdateCash(c.Player.ID, c.Player.GetCash())
