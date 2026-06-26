87ccffcf0643f3b598b0f4e604f1c24b3664d5f7
BXn4 <bencefodor@proton.me>
2026-06-26 19:17:31 +0200

    Added headers

--- internal/commands/editor/eex.go
+++ internal/commands/editor/eex.go
@@ -1,3 +1,9 @@
+/*
+CafeServerGo
+A custom TCP socket server hosting library / game server.
+Copyright (C) 2026 BXn4 and Hurka5
+*/
+
 package editor
 
 // min level = 5
--- internal/interfaces/tscafe.go
+++ internal/interfaces/tscafe.go
@@ -1,3 +1,9 @@
+/*
+CafeServerGo
+A custom TCP socket server hosting library / game server.
+Copyright (C) 2026 BXn4 and Hurka5
+*/
+
 package interfaces
 
 // TSCafe is a interface that exposes the Thread Safe functions of cafe.
